c23785df9a3684104c9b98d68231aded20f92c66
nisansala <nisansala.nx@gmail.com>
2018-07-30 23:58:29 +0530

    Update xxd.py

--- xxd.py
+++ xxd.py
@@ -6,15 +6,15 @@ while True:
     if not cont:
         break
     
-    step=('%07x'%(l*16))
+    col1=('%07x'%(l*16))
     
     ehex=d.encode('hex') 
     nehex=ehex
     dehex=[nehex[n:n+4] for n in range(0,len(nehex),4)]
-    cc=' '.join(map(str,dehex))
+    col2=' '.join(map(str,dehex))
 
-    b=ehex.decode('hex')
+    col3=ehex.decode('hex')
     
-    print('{0}: {1:<39} {2}'.format(step,''.join(cc),''.join(b)))
+    print('{0}: {1:<39} {2}'.format(col1,''.join(col2),''.join(col3)))
     l=l+1
     
