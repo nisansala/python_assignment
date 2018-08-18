24755f1f73441bf0ba03d67e0ad5e8bf321d0767
nisansala <nisansala.nx@gmail.com>
2018-08-18 03:31:30 -0700

    Update xxd2.py

--- xxd2.py
+++ xxd2.py
@@ -7,20 +7,21 @@ def xxd_file(file_path):
     count=0
     
     while True:
-        cont=f.read(16)
+        cont=fle.read(16)
         data=cont
         if not cont:
             break
     
         col1=('%08x'%(count*16))
     
-        ehex=d.encode('hex') 
+        ehex=data.encode('hex') 
         new_hex=ehex
         dehex=[new_hex[n:n+4] for n in range(0,len(new_hex),4)]
         col2=' '.join(map(str,dehex))
 
         col3=ehex.decode('hex')
-	    
+		
+    
         print('{0}: {1:<39} {2}'.format(col1,''.join(col2),''.join([col3 if col3 in string.printable[:-5] else '.' for col3 in data])))
 
         count += 1
