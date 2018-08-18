f5832c17714071a5526e9764eccb10cc2b42f49d
nisansala <nisansala.nx@gmail.com>
2018-08-18 15:39:30 +0530

    Update xxd2.py

--- xxd2.py
+++ xxd2.py
@@ -20,8 +20,7 @@ def xxd_file(file_path):
         col2=' '.join(map(str,dehex))
 
         col3=ehex.decode('hex')
-		
-    
+	    
         print('{0}: {1:<39} {2}'.format(col1,''.join(col2),''.join([col3 if col3 in string.printable[:-5] else '.' for col3 in data])))
 
         count += 1
