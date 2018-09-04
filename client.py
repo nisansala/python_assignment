9be592b364052d651b976152ad4784fd8156d0b1
nisansala <nisansala.nx@gmail.com>
2018-09-04 18:45:00 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,23 @@
+import sys, socket
+from threading import Thread
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.connect((host, port))
+
+
+
+def CL():
+	while True:
+		
+		sdata=soc.recv(1024)
+		print("server : "+sdata)
+
+th2 = Thread(target=CL).start()
+
+while True:
+
+	data = raw_input()
+	soc.send("client : "data) 
---
+++ server.py
@@ -0,0 +1,26 @@
+import sys, socket
+from threading import Thread
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+soc.bind((host, port))
+soc.listen(1)
+cli, addr = soc.accept()
+
+
+
+def SE():
+	while True:
+		
+		cdata=cli.recv(1024)
+		print("client : "+cdata)
+
+th = Thread(target=SE).start()
+
+while True:
+
+	data = raw_input()
+	cli.send("server : "data) 
